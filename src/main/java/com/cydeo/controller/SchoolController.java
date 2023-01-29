25f7a44af0a872fe74bc858252510ab6977c4e65
saityagci <saityagci@gmail.com>
2023-01-28 21:41:49 -0500

    getStudent added

--- src/main/java/com/cydeo/controller/SchoolController.java
+++ src/main/java/com/cydeo/controller/SchoolController.java
@@ -1,8 +1,13 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
+import org.apache.catalina.filters.AddDefaultCharsetFilter;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -10,14 +15,21 @@ import java.util.List;
 @RestController
 public class SchoolController {
     private final TeacherService teacherService;
+    private final StudentService studentService;
 
-    public SchoolController(TeacherService teacherService) {
+    public SchoolController(TeacherService teacherService, StudentService studentService) {
         this.teacherService = teacherService;
+        this.studentService = studentService;
     }
 
     @GetMapping("/teachers")
     public List<TeacherDTO> allTeachers(){
 
         return teacherService.findAll();
+    }
+    @GetMapping("/student")
+    public ResponseEntity<ResponseWrapper> readAllStudent(){
+        return ResponseEntity.ok(new ResponseWrapper("Student are successfully retrieve",
+                studentService.findAll()));
     }
 }
