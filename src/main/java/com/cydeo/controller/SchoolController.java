0744fd573d5c3e7d71e0758e8f0ce326b7bf4ce8
saityagci <saityagci@gmail.com>
2023-01-28 21:55:55 -0500

    parent endpoint added

--- src/main/java/com/cydeo/controller/SchoolController.java
+++ src/main/java/com/cydeo/controller/SchoolController.java
@@ -2,9 +2,11 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.ParentService;
 import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
 import org.apache.catalina.filters.AddDefaultCharsetFilter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,10 +18,12 @@ import java.util.List;
 public class SchoolController {
     private final TeacherService teacherService;
     private final StudentService studentService;
+    private final ParentService parentService;
 
-    public SchoolController(TeacherService teacherService, StudentService studentService) {
+    public SchoolController(TeacherService teacherService, StudentService studentService, ParentService parentService) {
         this.teacherService = teacherService;
         this.studentService = studentService;
+        this.parentService = parentService;
     }
 
     @GetMapping("/teachers")
@@ -27,9 +31,18 @@ public class SchoolController {
 
         return teacherService.findAll();
     }
-    @GetMapping("/student")
-    public ResponseEntity<ResponseWrapper> readAllStudent(){
-        return ResponseEntity.ok(new ResponseWrapper("Student are successfully retrieve",
-                studentService.findAll()));
+//    @GetMapping("/student")
+//    public ResponseEntity<ResponseWrapper> readAllStudent(){
+//        return ResponseEntity.ok(new ResponseWrapper("Student are successfully retrieve",
+//                studentService.findAll()));
+//    }
+        @GetMapping("/parent")
+    public ResponseEntity<ResponseWrapper> readAllParents(){
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .header("Parents","Return")
+                .body(new ResponseWrapper("Parents are successfully retrieved",
+                        parentService.findAll()));
     }
+
+
 }
